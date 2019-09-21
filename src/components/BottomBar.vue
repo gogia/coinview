f98450cc10c26643c96f80322aaa1f5a0da684e2
RudeBootie <cgsrarude@gmail.com>
2019-09-21 17:07:16 -0500

    Added index.html

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-toolbar bottom absolute style="margin-bottom: 2rem; width: 100%" color="#FAB3A9"></v-toolbar>
+  <v-toolbar bottom absolute style="margin-bottom: 2rem; width: 100%" color="#FAB3A9">
+      <v-layout justify-center> 
+      <v-btn style="margin-right: 2rem">Add Coin</v-btn>
+      <v-btn style="margin-right: 2rem">List Coins</v-btn>
+      <v-btn style="margin-right: 2rem">Options</v-btn>
+      </v-layout>
+  </v-toolbar>
 </template>
 
 <script lang="ts">
