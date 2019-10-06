3bfa9c5e270867f06b313d5b9856e8ff68486050
gogia <rmldillard@gmail.com>
2019-10-05 23:05:22 -0500

    Got props basic props assigned to cards programmatically

--- src/components/CoinBox.vue
+++ src/components/CoinBox.vue
@@ -1,48 +1,43 @@
 <template>
-<div>
-    
-    <v-flex v-for="(myCoin, index) in coinList"
-    :key="index">
+  <v-container grid-list>
+    <v-flex v-for="(myCoin, index) in coinList" :key="index">
+      <!--         :coinName="coinList[index].name"
+      :coinSymbol="coinSymbol[index].symbol"-->
 
-        <!--         :coinName="coinList[index].name"
-        :coinSymbol="coinSymbol[index].symbol" -->
-
-        <CoinCard>
-
-        </CoinCard>
+      <CoinCard
+       :coinName="myCoin.name"
+       :coinSymbol="myCoin.symbol"
+      ></CoinCard>
     </v-flex>
+  </v-container>
 
-
-    <!--
+  <!--
     <v-btn @click=testFunc>Test</v-btn>
-    -->
-</div>   
+  -->
 </template>
 
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 import { coin } from "./coinObject";
-import CoinCard from "./CoinCard.vue"
+import CoinCard from "./CoinCard.vue";
 export default Vue.extend({
-    components:{
-        CoinCard
-    },
+  components: {
+    CoinCard
+  },
 
-    computed: {
-        coinList: {
-            get():  [] {
-                return this.$store.state.testCoins;
-            }
-        }
-    },
+  computed: {
+    coinList: {
+      get(): [] {
+        return this.$store.state.testCoins;
+      }
+    }
+  },
 
-    methods: {
-        testFunc(){
-            console.log(this.coinList);
-        }
-
-    },
-    
-})
+  methods: {
+    testFunc() {
+      console.log(this.coinList);
+    }
+  }
+});
 </script>
