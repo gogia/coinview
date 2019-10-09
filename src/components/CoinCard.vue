3a86b0cd5f264bafd6e20b5a9d679d1a45d97fcc
gogia <rmldillard@gmail.com>
2019-10-08 20:52:05 -0500

    Centered card text

--- src/components/CoinCard.vue
+++ src/components/CoinCard.vue
@@ -5,8 +5,12 @@
     height="20rem"
     width="17rem"
   >
+  <v-layout justify-center>
     <v-card-title>{{ this.coinName }} | {{ this.coinSymbol }}</v-card-title>
-    <span>{{ this.currentPrice }} | {{ this.hourChange }} </span>
+  </v-layout>
+    <v-layout justify-center>
+    <span>{{ this.currentPrice }}  |  {{ this.hourChange }} </span>
+    </v-layout>
   </v-card>
 </template>
 
